61db56b2f10fefd5bbbcb26223e4a9802959180b
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-11-16 23:30:51 -0500

    chore :Iniciando Proyecto

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async register(name: string, email: string, password: string){
+    const user = await this.usersService.createUser(name, email, password);
+    return user;
+  }
+
+
+  async login(email: string, password: string){
+    const user = this.usersService.findByEmail(email);
+    if(!user) throw new UnauthorizedException('Credenciales inválidas');
+
+    const check = await bcrypt.compare(password, user.password);
+    if(!check) throw new UnauthorizedException('Credenciales inválidas');
+
+    const payload = { sub: user.id, role: user.role};
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    };
+  }
+
+
+}
---
+++ src/auth/guards/RolesGuard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<String[]>(
+      ROLES_KEY,
+      [ctx.getHandler(), ctx.getClass()],
+    );
+
+    if (!requiredRoles) return true;
+
+    const user = ctx.switchToHttp().getRequest().user;
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new Error('No tienes permisos para acceder a este recurso');
+    }
+    return true;
+  }
+}
